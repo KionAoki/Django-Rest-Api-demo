88ddfe357cd4b82efdb935919e9fb9732f0c249c
KionAoki Wang <kionaokiwang@KionAokideMacBook-Pro.local>
2022-12-01 01:34:39 +0800

    Partice Get API

--- drf/backend/api/views.py
+++ drf/backend/api/views.py
@@ -1,6 +1,17 @@
+import json
 from django.http import JsonResponse
 
 # Create your views here.
 
-def api_home(requests,*args,**kwargs):
-  return JsonResponse({"message":"Hi there, this is your Django API response!!"})
+def api_home(request,*args,**kwargs):
+  body = request.body
+  data = {}
+  try:
+    data = json.loads(body) # string of JSon data -> Python Dict
+  except:
+    pass
+  print(data)
+  print(request.GET) # url query params
+  print(request.headers)
+  data['content_type'] = request.content_type
+  return JsonResponse(data)
--- drf/py_client/basic.py
+++ drf/py_client/basic.py
@@ -2,7 +2,6 @@ import requests
 
 endpoint = "http://127.0.0.1:8000/api"
 
-get_response = requests.get(endpoint,json={"query":"Hello world"})
-print(get_response.text)
+get_response = requests.get(endpoint,params={"abc":123},json={"query":"Hello world"})
 print(get_response.status_code) 
-print(get_response.json()['message'])
+print(get_response.json())
