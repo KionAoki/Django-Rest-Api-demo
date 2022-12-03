8d3dd1a94e66b4490b8e752dcfe33479813c5bd8
KionAoki Wang <kionaokiwang@KionAokideMacBook-Pro.local>
2022-12-03 20:29:27 +0800

    Add rest framework's library into views

--- drf/backend/api/views.py
+++ drf/backend/api/views.py
@@ -1,13 +1,16 @@
 import json
-from django.http import JsonResponse
 from django.forms.models import model_to_dict
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 from products.models import Product
 # Create your views here.
 
+@api_view(['GET']) # 定義允許的 API request
 def api_home(request,*args,**kwargs):
   model_data = Product.objects.all().order_by("?").first()
   data = {}
   if model_data:
     data=model_to_dict(model_data)
-  return JsonResponse(data)
+  return Response(data)
